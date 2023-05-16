62cc566875d86b7a8dec985ece99390436311ac4
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-16 06:48:43 -0500

    Feat: add initial vendor serializer

---
+++ app/serializers/vendor_serializer.rb
@@ -0,0 +1,16 @@
+class VendorSerializer
+  def self.format_vendor(vendor)
+    {
+      data: vendors.map do |vendor|
+        {
+          id: vendor.id,
+          name: vendor.name,
+          description: vendor.description,
+          contact_name: vendor.contact_name,
+          contact_phone: vendor.contact_phone,
+          credit_accepted: vendor.credit_accepted
+        }
+      end
+    }
+  end
+end
