450f97165af1f2070bdf441edc2ca7dba5ff260e
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-16 07:32:49 -0500

    Test: add test for markets request

---
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'Markets API', type: :request do
+  it 'sends a list of markets' do
+    create_list(:market, 5)
+
+    get 'api/v0/markets'
+
+    expect(response).to be_successful
+
+    markets = JSON.parse(response.body, symbolize_names: true)
+
+    expect(markets.count).to eq(5)
+
+    markets.each do |market|
+      expect(market).to have_key(:data)
+      expect(market[:data]).to be_an(Array)
+
+      expect(market[:data]).to have_key(:id)
+      expect(market).to be_an(Integer)
+
+      expect(market[:data]).to have_key(:type)
+      expect(market[:data][:type]).to eq('market')
+
+      expect(market[:data]).to have_key(:attributes)
+      expect(market[:data][:attributes]).to be_a(Hash)
+
+      expect(market[:data][:attributes]).to have_key(:name)
+      expect(market[:data][:attributes][:name]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:street)
+      expect(market[:data][:attributes][:street]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:city)
+      expect(market[:data][:attributes][:city]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:county)
+      expect(market[:data][:attributes][:county]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:state)
+      expect(market[:data][:attributes][:state]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:zip)
+      expect(market[:data][:attributes][:zip]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:lat)
+      expect(market[:data][:attributes][:lat]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:lon)
+      expect(market[:data][:attributes][:lon]).to be_a(String)
+
+      expect(market[:data][:attributes]).to have_key(:vendor_count)
+      expect(market[:data][:attributes][:vendor_count]).to be_a(Integer)
+    end
+  end
+end
