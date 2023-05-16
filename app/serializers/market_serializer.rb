777a2e073b8df2f59e1bda4ef5b81782068772a2
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-16 06:56:31 -0500

    Fix: formatting of market serializer and refactor

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,17 +1,35 @@
 class MarketSerializer
   def self.format_markets(markets)
-    markets.map do |market|
-      {
-        id: market.id,
+    {
+      data: markets.map do |market|
+        market_hash(market)
+      end
+    }
+  end
+
+  def self.format_market(market)
+    {
+      data: market_hash(market)
+    }
+  end
+
+  private
+
+  def market_hash(market)
+    {
+      id: market.id,
+      type: 'market',
+      attributes: {
         name: market.name,
         street: market.street,
         city: market.city,
+        county: market.county,
         state: market.state,
         zip: market.zip,
         lat: market.lat,
         lon: market.lon,
         vendor_count: market.vendor_count
       }
-    end
+    }
   end
 end
