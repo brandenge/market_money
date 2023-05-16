54e3b532c7d98d627a88e0b422abab73c2535075
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-16 07:01:37 -0500

    Fix: formatting of vendor serializer and refactor

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -1,16 +1,23 @@
 class VendorSerializer
   def self.format_vendor(vendor)
     {
-      data: vendors.map do |vendor|
-        {
-          id: vendor.id,
-          name: vendor.name,
-          description: vendor.description,
-          contact_name: vendor.contact_name,
-          contact_phone: vendor.contact_phone,
-          credit_accepted: vendor.credit_accepted
-        }
-      end
+      data: vendor_hash(vendor)
+    }
+  end
+
+  private
+
+  def vendor_hash(vendor)
+    {
+      id: vendor.id,
+      type: 'vendor',
+      attributes: {
+        name: vendor.name,
+        description: vendor.description,
+        contact_name: vendor.contact_name,
+        contact_phone: vendor.contact_phone,
+        credit_accepted: vendor.credit_accepted
+      }
     }
   end
 end
