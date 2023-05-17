aa03475a41aad5f7cae1373506a49111658a433e
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-17 07:23:43 -0500

    Refactor: vendor serializer to use jsonapi-serializer

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -1,23 +1,9 @@
 class VendorSerializer
-  def self.format_vendor(vendor)
-    {
-      data: vendor_hash(vendor)
-    }
-  end
+  include JSONAPI::Serializer
 
-  private
-
-  def vendor_hash(vendor)
-    {
-      id: vendor.id,
-      type: 'vendor',
-      attributes: {
-        name: vendor.name,
-        description: vendor.description,
-        contact_name: vendor.contact_name,
-        contact_phone: vendor.contact_phone,
-        credit_accepted: vendor.credit_accepted
-      }
-    }
-  end
+  attributes :name,
+             :description,
+             :contact_name,
+             :contact_phone,
+             :credit_accepted
 end
