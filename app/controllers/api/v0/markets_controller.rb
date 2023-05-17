7f76dfae408871ac103178db421238f38b490301
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-17 07:18:30 -0500

    Refactor: market serializer to use jsonapi-serializer

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -3,12 +3,12 @@ class Api::V0::MarketsController < ApplicationController
 
   def index
     markets = Market.all
-    render json: MarketSerializer.new(markets).format_markets
+    render json: MarketSerializer.new(markets).serializable_hash.to_json
   end
 
   def show
     market = Market.find(params[:id])
-    render json: MarketSerializer.new(market).format_market
+    render json: MarketSerializer.new(market).serializable_hash.to_json
   end
 
   private
--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,39 +1,16 @@
 class MarketSerializer
-  def initialize(market_data)
-    @market_data = market_data
-  end
+  include JSONAPI::Serializer
 
-  def format_markets
-    {
-      data: @market_data.map do |market|
-        market_hash(market)
-      end
-    }
-  end
+  attributes :name,
+             :street,
+             :city,
+             :county,
+             :state,
+             :zip,
+             :lat,
+             :lon
 
-  def format_market
-    {
-      data: market_hash(@market_data)
-    }
-  end
-
-  private
-
-  def market_hash(market)
-    {
-      id: market.id.to_s,
-      type: 'market',
-      attributes: {
-        name: market.name,
-        street: market.street,
-        city: market.city,
-        county: market.county,
-        state: market.state,
-        zip: market.zip,
-        lat: market.lat,
-        lon: market.lon,
-        vendor_count: market.vendor_count
-      }
-    }
+  attribute :vendor_count do |market|
+    market.vendor_count
   end
 end
