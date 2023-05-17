34d8b3e9539b3dc9f166f66ba010745f30f0a5c2
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-17 11:46:24 -0500

    Test: add requre and include for header fixture

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,9 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+require './spec/fixtures/headers.rb'
+include Headers
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,7 +41,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
