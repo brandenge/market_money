b8dce5e560bcc0406944c14b01adf607c86991d6
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-17 02:19:18 -0500

    Feat: add sad path for market show action, and use rescue_from callback

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,11 +1,19 @@
 class Api::V0::MarketsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   def index
     markets = Market.all
-    render json: MarketSerializer.new.format_markets(markets)
+    render json: MarketSerializer.new(markets).format_markets
   end
 
   def show
     market = Market.find(params[:id])
-    render json: MarketSerializer.new.format_market(market)
+    render json: MarketSerializer.new(market).format_market
+  end
+
+  private
+
+  def not_found(exception)
+    render json: ErrorSerializer.new(exception).format_not_found, status: :not_found
   end
 end
