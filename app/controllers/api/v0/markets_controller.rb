574a42aa51a818868771a808d5e6ecfe191c89b1
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-17 00:05:08 -0500

    Feat: add market request

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,6 +1,11 @@
 class Api::V0::MarketsController < ApplicationController
   def index
     markets = Market.all
-    render json: MarketSerializer.format_markets(markets)
+    render json: MarketSerializer.new.format_markets(markets)
+  end
+
+  def show
+    market = Market.find(params[:id])
+    render json: MarketSerializer.new.format_market(market)
   end
 end
