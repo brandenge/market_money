c06873844a849d22fd56c5760d1e6d6860bd333a
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-17 09:04:51 -0500

    Refactor: rescue_from callback and helper method moved to application controller

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,6 +1,4 @@
 class Api::V0::MarketsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-
   def index
     markets = Market.all
     render json: MarketSerializer.new(markets).serializable_hash.to_json
@@ -9,11 +7,5 @@ class Api::V0::MarketsController < ApplicationController
   def show
     market = Market.find(params[:id])
     render json: MarketSerializer.new(market).serializable_hash.to_json
-  end
-
-  private
-
-  def not_found(error)
-    render json: ErrorSerializer.new(error).format_not_found, status: :not_found
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(error)
+    render json: ErrorSerializer.new(error).format_not_found, status: :not_found
+  end
 end
