d2051740dbb991581a8f960b8ed5a5375308529d
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-18 07:46:06 -0500

    Refactor: organize expect statements

--- spec/requests/api/v0/destroy_vendor_spec.rb
+++ spec/requests/api/v0/destroy_vendor_spec.rb
@@ -8,14 +8,13 @@ RSpec.describe 'Destroy Vendor API', type: :request do
   context 'using a valid vendor id' do
     it 'can destroy a vendor' do
       expect(Vendor.count).to eq(1)
-
       expect{ delete api_v0_vendor_path(@vendor), headers: JSON_HEADER }
         .to change(Vendor, :count).by(-1)
+      expect(Vendor.count).to eq(0)
 
       expect(response).to be_successful
       expect(response.status).to eq(204)
 
-      expect(Vendor.count).to eq(0)
       expect{ Vendor.find(@vendor.id) }
         .to raise_error(ActiveRecord::RecordNotFound)
     end
@@ -23,14 +22,13 @@ RSpec.describe 'Destroy Vendor API', type: :request do
 
   context 'using an invalid vendor id' do
     it 'sends error details for invalid id' do
-      expect(Vendor.count).to eq(1)
-
       invalid_id = 123123123123
 
+      expect(Vendor.count).to eq(1)
       expect{ delete api_v0_vendor_path(invalid_id), headers: JSON_HEADER }
       .to change(Vendor, :count).by(0)
-
       expect(Vendor.count).to eq(1)
+      
       expect{ Vendor.find(@vendor.id) }
       .to_not raise_error
 
