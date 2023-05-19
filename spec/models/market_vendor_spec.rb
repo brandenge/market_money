dc15adb9adf1f414b3749e3e865a9156255f452c
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-19 02:29:00 -0500

    Test: add test for custom validation for MarketVendor

--- spec/models/market_vendor_spec.rb
+++ spec/models/market_vendor_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe MarketVendor, type: :model do
     it { should validate_numericality_of(:market_id) }
     it { should validate_numericality_of(:vendor_id) }
   end
+
+  describe 'instance methods' do
+    describe '#is_unique' do
+      it 'does not raise an error if the vendor has not already been added to the market' do
+        market = create(:market)
+        vendor = create(:vendor)
+
+        expect{ MarketVendor.create!(market_id: market.id, vendor_id: vendor.id) }.to_not raise_error
+      end
+
+      it 'raises an error if the vendor has already been added to the market' do
+        market = create(:market)
+        vendor = create(:vendor)
+        market_vendor = create(:market_vendor, market: market, vendor: vendor)
+
+        expect{ MarketVendor.create!(market_id: market.id, vendor_id: vendor.id) }.to raise_error(CustomError)
+      end
+    end
+  end
 end
