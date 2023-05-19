1e82558953ec024fd54cbbd7ae0f096c5cb76809
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-18 20:31:24 -0500

    Feat: add custom validator to market vendor model

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -4,4 +4,13 @@ class MarketVendor < ApplicationRecord
 
   validates :market_id, presence: true, numericality: true
   validates :vendor_id, presence: true, numericality: true
+
+  validate :is_unique
+
+  def is_unique
+    if MarketVendor.where(market_id: market_id)
+                   .where(vendor_id: vendor_id).count > 0
+      raise CustomError.new("Validation failed: Market vendor association between market with market_id=#{market_id} and vendor_id=#{vendor_id} already exists", 422)
+    end
+  end
 end
