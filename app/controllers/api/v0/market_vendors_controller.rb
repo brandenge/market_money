6fb69aefcbc6f893909f85d26e850977f41a57dd
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-18 20:34:07 -0500

    Feat: add create market vendor

---
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -0,0 +1,20 @@
+class Api::V0::MarketVendorsController < ApplicationController
+  def create
+    if params[:market_vendor][:market_id].nil? ||
+      params[:market_vendor][:vendor_id].nil?
+      MarketVendor.create!(strong_params)
+    end
+    begin
+      MarketVendor.create!(strong_params)
+      render json: MarketVendorSerializer.new.successfully_added, status: 201
+    rescue ActiveRecord::RecordInvalid => e
+      render json: ErrorSerializer.new(e).serialize_json, status: 404
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:market_vendor).permit(:market_id, :vendor_id)
+  end
+end
